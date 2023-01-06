80fee1a2564215bf64bef6329424c2ff4f483176
adityadevcode <87442837+adityadevcode@users.noreply.github.com>
2023-01-06 03:03:27 -0500

    initial commit

---
+++ sample.js
@@ -0,0 +1,55 @@
+// var, let , const
+// define, update, redifine
+// const cannot mutate primitive type
+
+// define
+// var number = 100;
+// console.log(number);
+// // update
+// number = 200;
+// console.log(number);
+// // redifine
+// var number = 'orange';
+// console.log(number);
+
+// let amount = 100;
+// console.log(amount);
+// amount = 200;
+// console.log(amount);
+// amount = 'orange';
+// console.log(amount);
+
+// const total = 100;
+// // console.log(total);
+// // total = 200;
+// const person = { name: 'bob' };
+// person.name = 'john';
+// console.log(person); //john
+// person.name ="rowe";
+// console.log(person);//rowe
+// console.log(person.name);//rowe
+
+// const family = {
+// nam:'tim',
+// age:34,
+// place: 'some'
+// }
+// console.log(family); //{nam: 'tim', age: 34, place: 'some'}
+// console.log(family.nam);
+// family.nam ="krush"
+// console.log(family.nam); //krush
+
+////////////////////////////////////////
+//var, let, const
+//let const blocked scoped -any thing with in brackets
+// var function scoped
+
+// global scope
+// console.log(pla);
+// var pla="somewhere"  //undefined
+
+// console.log(pl);
+// let pl="some" //cannot access pl before intialization
+
+// console.log(la);
+// const la="place" //cannot access pl before intialization
